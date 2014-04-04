b05cd347cce751cdc517c3635fab7d2b9739bad9
deathcap <deathcap@gmx.co.uk>
2014-04-03 22:20:55 -0700

    Fix slot indexing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var createArtpacks = require('artpacks');
 var ndarray = require('ndarray');
 var inherits = require('inherits');
 var EventEmitter = require('events').EventEmitter;
+var fill = require('ndarray-fill');
 
 module.exports = function(game, opts) {
   return new StitchPlugin(game, opts);
@@ -22,15 +23,20 @@ function StitchPlugin(game, opts) {
 
   // texture atlas width and height
   // MAX_TEXTURE_SIZE at http://webglstats.com/, 100% of WebGL users support 2048x2048
-  this.atlasSize = opts.atlasSize !== undefined ? opts.atlasSize : 2048;
+  this.atlasSize = opts.atlasSize !== undefined ? opts.atlasSize : 256;//2048; // requires downsampling each tile even if empty :( so make it smaller
   this.tileSize = opts.tileSize !== undefined ? opts.tileSize : 16;
-  this.tileCount = opts.atlasSize / opts.tileSize; // each dimension
+  this.tileCount = this.atlasSize / this.tileSize; // each dimension
 
   // 5-dimensional array of tiles compatible with:
   //  https://github.com/mikolalysenko/tile-mip-map
   //  https://github.com/mikolalysenko/gl-tile-map
   // [rows, columns, tile height, tile width, channels]
-  this.atlas = ndarray(new Uint8Array(this.atlasSize * this.atlasSize * 4), [this.tileCount, this.tileCount, this.tileSize, this.tileSize, 4]); // RGBA
+  this.atlas = ndarray(new Uint8Array(this.atlasSize * this.atlasSize * 4),
+      [this.tileCount, this.tileCount, this.tileSize, this.tileSize, 4]
+      // TODO: strides?
+      );
+  fill(this.atlas, function(row,col,y,x,c) { return 80; }); // fill background for easier debugging TODO
+
   this.nextX = this.nextY = 0;
   this.pending = 0;
 
@@ -69,12 +75,17 @@ StitchPlugin.prototype.stitch = function() {
             var x = pixels.get(i, j, k);
 
             self.atlas.set(self.nextX, self.nextY, i, j, k, x);
-            self.nextX += 1;
-            if (self.nextX >= self.atlasSize / self.tileSize) {
-              self.nextX = 0;
-              self.nextY += 1; // TODO: instead, add to 4-dimensional strip then recast as 5-d?
-            }
           }
+        }
+      }
+      // point to next slot
+      self.nextX += 1;
+      if (self.nextX >= self.tileCount) {
+        self.nextX = 0;
+        self.nextY += 1; // TODO: instead, add to 4-dimensional strip then recast as 5-d?
+        if (self.nextY >= self.tileCount) {
+          throw new Error('texture sheet full! '+self.tileCount+'x'+self.tileCount+' exceeded');
+          // TODO: 'flip' the texture sheet, see https://github.com/deathcap/voxel-texture-shader/issues/2
         }
       }
 
